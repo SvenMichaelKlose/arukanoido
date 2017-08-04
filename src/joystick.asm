32b0e78f6fa5ba79baa0cf2a5a354e8edb262ff4
Sven Michael Klose <pixel@copei.de>
2017-08-04 14:32:28 +0200

    wait_fire: Also deal with paddle.

--- src/joystick.asm
+++ src/joystick.asm
@@ -1,18 +1,13 @@
-; For debugging.
-
+; Wait for joystick or paddle fire.
 wait_fire:
-l:  lda #0              ; Fetch joystick status.
+l:  lda #0
     sta $9113
     lda $9111
     tax
     and #joy_fire
+    beq +n
+    txa
+    and #joy_left
     bne -l
 
-l:  lda #0              ; Fetch joystick status.
-    sta $9113
-    lda $9111
-    tax
-    and #joy_fire
-    beq -l
-
-    rts
+n:  rts
