563c4470cf1ebdc0c066302a3b954a2fc9e79cd2
Sven Michael Klose <pixel@copei.de>
2017-08-06 11:53:44 +0200

    Wait until fire button has been released.

--- src/joystick.asm
+++ src/joystick.asm
@@ -5,9 +5,23 @@ l:  lda #0
     lda $9111
     tax
     and #joy_fire
-    beq +n
+    beq wait_joystick_fire_released
     txa
     and #joy_left
     bne -l
 
-n:  rts
+wait_paddle_fire_released:
+l:  lda #0
+    sta $9113
+    lda $9111
+    and #joy_left
+    beq -l
+    rts
+
+wait_joystick_fire_released:
+l:  lda #0
+    sta $9113
+    lda $9111
+    and #joy_fire
+    beq -l
+    rts
