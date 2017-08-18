f48265e6022308122a1d585e61407b0c89c20299
Sven Michael Klose <pixel@copei.de>
2017-08-18 10:36:04 +0200

    Display converted intro animation.

--- media/round-intro/toplevel.lisp
+++ media/round-intro/toplevel.lisp
@@ -1,9 +1,9 @@
-(defvar video_width (* 21 4))
-(defvar video_height (* 22 8))
+(defvar video_width (* 15 4))
+(defvar video_height (* 32 8))
 (defvar edges_width video_width)
 (defvar edges_height video_height)
 (defvar *dithering* nil)
-(defvar *errdec* 0.222222) ;0.333333)
+(defvar *errdec* 0) ;0.222222) ;0.333333)
 
 (defconstant +bw-palette+
              '((  0   0   0)
@@ -11,20 +11,29 @@
 
 (defconstant +vic20-micropalette+
              '((  0   0   0)    ; Black
-;                  (231 146 147)     ; Light Red
-               (182  31  33)    ; Red
-               (77  240 255)    ; Cyan
-               (68  226  55)))  ; Green
+               (#xb6  #x1f  #x21)    ; Red
+               (#x4d  #xf9 #xff)    ; Cyan
+               (#x1a   #x34 #xff)))    ; Blue
+;               (68  226  55)))  ; Green
+
+;"#CA5400"
+;"#E9B072"
+;"#E79293"
+;"#9AF7FD"
+;"#E09FFF"
+;"#8FE493"
+;"#8290FF"
+;"#E5DE85"
 
 (defconstant +vic20-palette+
              '((  0   0   0)    ; Black
                (255 255 255)    ; White
-               (182  31  33)    ; Red
-               (77  240 255)    ; Cyan
-               (180  63 255)    ; Purple
-               (68  226  55)    ; Green
-               (26   52 255)    ; Blue
-               (220 215  27)))  ; Yellow
+               (#xb6  #x1f  #x21)    ; Red
+               (#x4d  #xf9 #xff)    ; Cyan
+               (#xb4  #x3f #xff)    ; Purple
+               (#x44  #xe2  #x37)    ; Green
+               (#x1a   #x34 #xff)    ; Blue
+               (#xdc #xd7  #x1b)))  ; Yellow
 
 
 (defconstant +vic20-palette-ext+
@@ -114,6 +123,10 @@
 
 (defun draw (video ctx ctx2 w h)
   (ctx.draw-image video 0 0 w h)
+  (= ctx.fill-style "#000")
+  (ctx.fill-rect 0 0 w 30)
+  (= ctx.fill-style "#fff")
+;  (ctx.fill-rect 0 0 w 100)
   (ctx2.draw-image video 0 0 w h)
   (= *dithering* (make-symbol (upcase (get-option (document.query-selector ".dithering")))))
   (with (data     (ctx.get-image-data 0 0 w h)
@@ -139,14 +152,12 @@
     (ctx2.put-image-data data2 0 0)
     (= *former-pixels* pixels)))
 
-(defun draw-new (video ctx ctx2 w h)
-  (draw video ctx ctx2 w h))
-
-(defun draw-update (video ctx ctx2 w h)
-  (set-timeout [draw-update video ctx ctx2 w h] (/ 1000 *fps*))
-  (draw-new video ctx ctx2 w h))
-
-(defvar *started?* nil)
+(defun draw-update (video ctx ctx2 w h frame)
+  (= video.src (+ "frames/file0" frame ".png"))
+  (= video.onload [0 (draw video ctx ctx2 w h)
+                       (when (< frame 552)
+                         (set-timeout [0 (draw-update video ctx ctx2 w h (++ frame))]
+                                      0))]))
 
 (defun set-image-smoothing (x v)
   (= x.image-smoothing-enabled v)
@@ -155,33 +166,27 @@
   (= x.ms-image-smoothing-enabled v))
 
 (defun setup-canvas (video canvas canvas2)
-  (video.add-event-listener "play"
-                            [0 (unless *started?*
-                                 (= *started?* t)
-                                 (= canvas.width video_width)
-                                 (= canvas.height video_height)
-                                 (= canvas.style.width "0")
-                                 (= canvas.style.height "0")
-                                 (= canvas2.width edges_width)
-                                 (= canvas2.height edges_height)
-                                 (= canvas2.style.width (+ (/ (* 22 8 6) 3) "px"))
-                                 (= canvas2.style.height (+ (* 23 8) "px")))
-                               (set-image-smoothing (canvas.get-context "2d") t)
-                               (set-image-smoothing (canvas2.get-context "2d") t)
-                               (draw-update this
-                                            (canvas.get-context "2d")
-                                            (canvas2.get-context "2d")
-                                            video_width video_height)]
-                            false))
+  (= canvas.width video_width)
+  (= canvas.height video_height)
+  (= canvas.style.width "0")
+  (= canvas.style.height "0")
+  (= canvas2.width edges_width)
+  (= canvas2.height edges_height)
+  (= canvas2.style.width (+ (* 8 edges_width) "px"))
+  (= canvas2.style.height (+ (* 3 edges_height) "px"))
+  (set-image-smoothing (canvas.get-context "2d") t)
+;  (set-image-smoothing (canvas2.get-context "2d") t)
+          (= video.style.width "50%")
+          (= video.style.height "50%")
+  (draw-update video
+               (canvas.get-context "2d")
+               (canvas2.get-context "2d")
+               video_width video_height
+               102))
 
 ((j-query window).load
     #'(()
         (= document.body.style "background: black; color: white;")
-        (let video (document.query-selector "video")
-          (= video.src "video.mp4") ;(window.*U-R-L.create-object-u-r-l localMediaStream))
-          (= video.onloadedmetadata #'((e)))
-          (= video.style.width "50%")
-          (= video.style.height "50%")
-          (setup-canvas video
+          (setup-canvas (document.query-selector "img")
                         (document.query-selector ".input")
-                        (document.query-selector ".edges")))))
+                        (document.query-selector ".edges"))))
