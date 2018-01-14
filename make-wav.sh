d9648961c8dcad89a745aa55b69292a82c38e9e0
Sven Michael Klose <pixel@copei.de>
2018-01-14 23:04:36 +0100

    Rename copied README.

--- make-wav.sh
+++ make-wav.sh
@@ -2,3 +2,4 @@
 
 sbcl --noinform --core bender/bender make-wav.lisp
 cp -v arukanoido*.zip NEWS ~/Desktop/hugbox.org/www/pixel/software/vic-20/arukanoido/
+cp -v README.md ~/Desktop/hugbox.org/www/pixel/software/vic-20/arukanoido/README.txt
