dc0838cebb7bd54fa34836754dd837ce96bc79cc
Sven Michael Klose <pixel@copei.de>
2018-02-11 10:36:17 +0100

    Remove temporary file when finished.

--- make-wav.sh
+++ make-wav.sh
@@ -3,3 +3,4 @@
 sbcl --noinform --core bender/bender make-wav.lisp
 cp -v arukanoido*.zip NEWS ~/Desktop/hugbox.org/www/pixel/software/vic-20/arukanoido/
 cp -v README.md ~/Desktop/hugbox.org/www/pixel/software/vic-20/arukanoido/README.txt
+rm tap2wav.tmp
