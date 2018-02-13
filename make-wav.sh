b32ff5d9880655ce2357065fc9e726eb5702ae91
Sven Michael Klose <pixel@copei.de>
2018-02-13 13:06:30 +0100

    File split.

--- make-wav.sh
+++ make-wav.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
 sbcl --noinform --core bender/bender make-wav.lisp
-#cp -v arukanoido*.zip NEWS ~/Desktop/hugbox.org/www/pixel/software/vic-20/arukanoido/
-#cp -v README.md ~/Desktop/hugbox.org/www/pixel/software/vic-20/arukanoido/README.txt
 rm tap2wav.tmp
---
+++ tosite.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cp -v arukanoido*.zip NEWS ~/Desktop/hugbox.org/www/pixel/software/vic-20/arukanoido/
+cp -v README.md ~/Desktop/hugbox.org/www/pixel/software/vic-20/arukanoido/README.txt
