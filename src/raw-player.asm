5c41cf8e1cad7e1e573819bbfcea8aa398ba77f9
Sven Michael Klose <pixel@copei.de>
2018-02-19 10:31:21 +0100

    Slightly faster NMI handler.

--- src/raw-player.asm
+++ src/raw-player.asm
@@ -46,15 +46,20 @@ raw_play_sample:
     lda digisound_a
     rti
 
+    ; Go to next page.
 n:  inc @(+ 1 raw_play_ptr)
     lda @(+ 1 raw_play_ptr)
     cmp #$c0
-    bne +r
-    lda #$a0
+    beq +n
+    ldy digisound_y
+    lda digisound_a
+    rti
+
+    ; Go to next bank.
+n:  lda #$a0
     sta @(+ 1 raw_play_ptr)
     inc $9ffe
-
-r:  ldy digisound_y
+    ldy digisound_y
     lda digisound_a
     rti
 
