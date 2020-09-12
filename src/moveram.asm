d728246341b979a45c2ea6a02a704d52e600c4cd
Sven Michael Klose <pixel@hugbox.org>
2020-09-12 22:49:51 +0200

    Fix counting bug.

--- src/moveram.asm
+++ src/moveram.asm
@@ -1,38 +1,38 @@
 moveram:
     ldy #0
     ldx c
-    sty c
-    inc @(++ c)
+    inx
+    inc @(+ c 1)
     cmp #0
     bne copy_backwards
 
-copy_forwards:
 l:  lda (s),y
     sta (d),y
     iny
     beq +k
+copy_forwards:
 q:  dex
     bne -l
-    dec @(++ c)
+    dec @(+ c 1)
     bne -l
     rts
 
-k:  inc @(++ s)
-    inc @(++ d)
+k:  inc @(+ s 1)
+    inc @(+ d 1)
     jmp -q
 
-copy_backwards:
 l2: lda (s),y
     sta (d),y
     dey
     cpy #$ff
     beq +m2
+copy_backwards:
 q2: dex
     bne -l2
-    dec @(++ c)
+    dec @(+ c 1)
     bne -l2
     rts
 
-m2: dec @(++ s)
-    dec @(++ d)
+m2: dec @(+ s 1)
+    dec @(+ d 1)
     jmp -q2
