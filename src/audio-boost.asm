f359d789b8ae0bb2f680a2665d222ce48ff62733
Sven Michael Klose <pixel@hugbox.org>
2020-09-27 17:36:06 +0200

    Full volume digi blast.

--- src/audio-boost.asm
+++ src/audio-boost.asm
@@ -1,16 +1,34 @@
 audio_boost:
-    lda #$b0
-    sta $900e
-    ldx #$7e
-    stx $900c
+    ; Stop alto and soprano channel.
+    lda #$7e
+    sta $900a
+    sta $900b
+    sta $900c
+    sta $900d
+
+    ; Let channels rest until LSFRs are empty.
     ldy #0
 l:  dey
     bne -l
+
+    ; Start alto channel at highest frequency of
+    ; ~8659Hz, which is 128 CPU cycles.
     lda #$fe
-    stx $900c
-    stx $900c
-    sta $900c
-    sta $900c
-    stx $900c
-    sta $900c
+    sta $900b
+
+    ; Waste 56 cycles.
+    ; 2+10*5+4
+    ldy #10     ; 2
+l:  dey         ; 2
+    bne -l      ; 3
+
+    ; Waste missing 2 cycles.
+    nop         ; 2
+
+    ; Start soprano channel at same frequency as alto.
+    ; Takes 6 cycles. Then it's exactly half a wave
+    ; (512 cycles) apart from the alto.
+    lda #$fd    ; 2
+    sta $900c   ; 4
+
     rts
