2b40dc2e55fec9f970908ea967dd613e36fd5ad4
Sven Michael Klose <pixel@hugbox.org>
2020-10-07 02:21:25 +0200

    Cut end of "game over" tune.

--- src/music-arcade-blk5.asm
+++ src/music-arcade-blk5.asm
@@ -1,4 +1,5 @@
 exm_round_intro:        @(fetch-file "obj-audio/round-intro.2.4000.exm")
 exm_round_start:        @(fetch-file "obj-audio/round-start.4.4000.exm")
 ;exm_reflection_med:     @(fetch-file "obj-audio/reflection-med.1.4000.rle")
-exm_game_over:          @(fetch-file "obj-audio/game-over.2.4000.exm")
+exm_game_over:          @(subseq (fetch-file "obj-audio/game-over.2.4000.exm")
+                                 0 #x1e10)
