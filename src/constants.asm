0f6913bbccd821dd49ef99923a803f3242632d81
Sven Michael Klose <pixel@hugbox.org>
2020-11-21 16:58:21 +0100

    Double ball speed for paddles.

--- src/constants.asm
+++ src/constants.asm
@@ -40,7 +40,7 @@ default_num_lifes       = 3
 default_ball_speed      = 5
 
 min_ball_speed          = 2
-max_ball_speed          = 8
+max_ball_speed          = 16
 max_ball_speed_joystick = 6
 max_ball_speed_joystick_top = 6
 
