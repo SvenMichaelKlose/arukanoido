540470a5713409f30e3893bb3ee988578dfa9ee3
Sven Michael Klose <pixel@hugbox.org>
2022-01-09 16:39:48 +0100

    Sketch pre-shifting all offsets of a sprite.

--- src/sprites-vic-huge-preshifted.asm
+++ src/sprites-vic-huge-preshifted.asm
@@ -1,13 +1,9 @@
-; Preshift sprite graphics for a single sprite.
-;
-; Destination must be zeroed out beforehand which is much
-; snappier if this routine does not have to do it but you
-; do it in one run for all graphics.
+; Preshift sprite graphics for a sprite.
 ;
 ; A: Number of bits to shift.
 ; X: Sprite index
 ; d: Destination address
-preshift_huge_sprite:
+preshift_huge_sprite_one_offset:
     ; Determine width.
     pha
     lda sprites_dimensions,x
@@ -88,3 +84,27 @@ step_to_next_column:
     bcc +n
     inc @(++ d)
 n:  rts
+
+; Preshift sprite graphics for a sprite for all offsets.
+;
+; Destination must be zeroed out beforehand which is much
+; snappier if this routine does not have to do it but you
+; do it in one run for all graphics.
+;
+; Will shift 4 times for multicoloured sprites and 8 times
+; for hires ones.
+;
+; X: Sprite index
+; d: Destination address
+preshift_huge_sprite:
+    ldy #0
+l:  tya
+    jsr preshift_huge_sprite_one_offset
+    lda sprites_c,x
+    iny
+    and #multicolor ; Multicolor flag?
+    beq +n          ; No, step on a single bit.
+    iny             ; Yes, step one two bits.
+n:  cpy #8
+    bne -l
+    rts
