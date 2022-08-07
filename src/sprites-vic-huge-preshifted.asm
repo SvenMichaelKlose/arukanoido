b524bc4e14e57f96bebae17064eb628267968807
Sven Michael Klose <pixel@hugbox.org>
2022-08-07 11:04:50 +0200

    Grand clean-up.

--- src/sprites-vic-huge-preshifted.asm
+++ src/sprites-vic-huge-preshifted.asm
@@ -1,78 +1,63 @@
 ; Preshift sprite graphics for a sprite.
 ;
 ; A: Number of bits to shift.
-; Y: Sprite dimensions (00YYYXXX)
+; draw_sprites_tmp: Sprite dimensions (00YYYXXX)
 ;    X: width of X axis
 ;    Y: height of Y axis
 ; s: Graphics address
 ; d: Destination address
 preshift_huge_sprite_one_offset:
-    pha
+    ; Configure the blitter.
+    sta @(++ blit_left_addr)
+    tay
+    lda negate7,y
+    sta @(++ blit_right_addr)
 
     ; Determine width.
     lda draw_sprites_tmp
     and #%111
     sta sprite_cols
-    sta sprite_cols_on_screen
-
-    pla
-    pha
-    cmp #0
-    beq +n
-    inc sprite_cols_on_screen     ; One more due to shift.
-n:
 
     ; Determine height.
     lda draw_sprites_tmp
     lsr
     lsr
     lsr
-    sta sprite_rows
-    sta sprite_rows_on_screen
-    asl
-    asl
-    asl
     sta sprite_lines
-    sta sprite_lines_on_screen
 
-    ; Configure the blitter.
-    pla
-    sta @(++ blit_left_addr)
-    tay
-    lda negate7,y
-    sta @(++ blit_right_addr)
-
-    ; Get sprite graphics.
-    lda sprites_gl,x
-    sta s
-    lda sprites_gh,x
-    sta @(++ s)
-
-    lda sprite_cols
-    sta draw_sprites_tmp2
+    ; Save sprite graphics pointer for right column.
+    lda s
+    sta tmp2
+    lda @(++ s)
+    sta tmp3
 
     ; Draw left half of sprite column.
 l:  ldy sprite_lines
     dey
     jsr _blit_right_loop
-
     jsr step_to_next_column
 
+    ; Check if we have to draw a right column.
+    lda @(++ blit_left_addr)
+    beq +n          ; No, not shifting…
+
     ; Draw right half of sprite column.
-    lda sprites_x,x
-    and #%111
-    beq +n
+    lda tmp2
+    sta s
+    lda tmp3
+    sta @(++ s)
     ldy sprite_lines
     dey
     jsr _blit_left_loop
-n:
+n:  dec sprite_cols
+    bne +n2
+    beq step_to_next_column
 
-    ; Break here when all columns are done…
-    dec draw_sprites_tmp2
-    beq +step_to_next_column
+n:  dec sprite_cols
+    beq +done
 
     ; Step to next sprite graphics column.
-    lda s
+n2: lda s
     clc
     adc sprite_lines
     sta s
@@ -83,17 +68,17 @@ n:
 step_to_next_column:
     lda d
     clc
-    adc sprite_lines_on_screen
+    adc sprite_lines
     sta d
-    bcc +n
+    bcc +done
     inc @(++ d)
-n:  rts
+
+done:
+    rts
 
 ; Preshift sprite graphics for a sprite for all offsets.
 ;
-; Destination must be zeroed out beforehand which is much
-; snappier if this routine does not have to do it but you
-; do it in one run for all graphics.
+; Destination must be zeroed out beforehand.
 ;
 ; Will shift 4 times for multicoloured sprites and 8 times
 ; for hires ones.
@@ -105,10 +90,11 @@ n:  rts
 preshift_huge_sprite:
     sty draw_sprites_tmp
     stx draw_sprites_tmp2
+
     ldy #0
 l:  lda s
     pha
-    lda @(+ s 1)
+    lda @(++ s)
     pha
     tya
     pha
@@ -116,9 +102,10 @@ l:  lda s
     pla
     tay
     pla
-    sta @(+ s 1)
+    sta @(++ s)
     pla
     sta s
+
     iny
     lda draw_sprites_tmp2
     beq +n          ; Hires, step on a single bit.
