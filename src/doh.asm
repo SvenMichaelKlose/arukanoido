beae05f1d737818d11a5a460816149cfa67dcddf
Sven Michael Klose <pixel@hugbox.org>
2023-08-25 07:00:34 +0200

    Clean up.
    Add names constant 'first_doh_char' and comments.

--- src/doh.asm
+++ src/doh.asm
@@ -1,24 +1,26 @@
+first_doh_char = @(+ foreground (half foreground))
+
 init_doh_charset:
     ldx #@(* 15 16)
 l:  lda @(-- gfx_doh_a),x
-    sta @(-- (+ charset (* 8 (+ foreground (half foreground))))),x
+    sta @(-- (+ charset (* 8 first_doh_char))),x
     lda @(-- gfx_doh_b),x
-    sta @(-- (+ charset (* 8 (+ framechars foreground (half foreground))))),x
+    sta @(-- (+ charset (* 8 (+ framechars first_doh_char)))),x
     dex
     bne -l
     rts
 
 draw_doh:
-    lda #@(+ foreground (half foreground))
+    lda #@first_doh_char
     sta tmp
     lda #5
     sta scrx
-    lda #5
+    lda #5      ; (number of columns)
     sta tmp3
 
 l2: lda #7
     sta scry
-    lda #12
+    lda #12     ; (number of rows)
     sta tmp2
 l:  jsr scrcoladdr
     lda tmp
@@ -28,9 +30,9 @@ l:  jsr scrcoladdr
     inc scry
     inc tmp
     lda tmp
-    cmp #@(+ foreground (half foreground) 30)
+    cmp #@(+ first_doh_char 30)
     bne +n
-    lda #@(+ framechars foreground (half foreground))
+    lda #@(+ framechars first_doh_char)
     sta tmp
 n:  dec tmp2
     bne -l
