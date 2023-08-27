f4bf5aed566f94552ab4142e9954844cb331637f
Sven Michael Klose <pixel@hugbox.org>
2023-08-27 18:33:15 +0200

    Better intro sound with PRG, worse for ROM tp make space for init.

--- src/music-arcade-blk5.asm
+++ src/music-arcade-blk5.asm
@@ -1,4 +1,10 @@
-exm_round_intro:        @(fetch-file "obj-audio/round-intro.2.4000.exm")
+exm_round_intro:
+if @*rom?*
+    @(fetch-file "obj-audio/round-intro.4.4000.exm")
+end
+if @(not *rom?*)
+    @(fetch-file "obj-audio/round-intro.2.4000.exm")
+end
 exm_round_start:        @(fetch-file "obj-audio/round-start.2.4000.exm")
 ;exm_reflection_med:     @(fetch-file "obj-audio/reflection-med.1.4000.rle")
 exm_game_over:          @(subseq (fetch-file "obj-audio/game-over.2.4000.exm")
