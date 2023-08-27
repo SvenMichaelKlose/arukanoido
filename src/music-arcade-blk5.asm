c26abf3158743269162154e326635471645bde4c
Sven Michael Klose <pixel@hugbox.org>
2023-08-27 20:20:37 +0200

    Less quality of game over sound so NTSC won't hiccup.

--- src/music-arcade-blk5.asm
+++ src/music-arcade-blk5.asm
@@ -3,9 +3,9 @@ if @*rom?*
     @(fetch-file "obj-audio/round-intro.4.4000.exm")
 end
 if @(not *rom?*)
-    @(fetch-file "obj-audio/round-intro.2.4000.exm")
+    @(fetch-file "obj-audio/round-intro.3.4000.exm")
 end
-exm_round_start:        @(fetch-file "obj-audio/round-start.2.4000.exm")
+exm_round_start:        @(fetch-file "obj-audio/round-start.3.4000.exm")
 ;exm_reflection_med:     @(fetch-file "obj-audio/reflection-med.1.4000.rle")
-exm_game_over:          @(subseq (fetch-file "obj-audio/game-over.2.4000.exm")
+exm_game_over:          @(subseq (fetch-file "obj-audio/game-over.3.4000.exm")
                                  0 #x1e10)
