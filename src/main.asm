9f20b56478d8f4f3079dc9030ed266fa32bf6305
Sven Michael Klose <pixel@hugbox.org>
2023-10-16 20:49:27 +0200

    Add Mike to the credits for his DOH graphics.

--- src/main.asm
+++ src/main.asm
@@ -252,7 +252,7 @@ draw_credits:
 
     lda playfield_yc
     clc
-    adc #3
+    adc #1
     sta scry
 
     0
@@ -261,7 +261,7 @@ draw_credits:
     lday <txt_press >txt_press
     call <print_string_ay >print_string_ay
 
-    addzbi scry 4
+    addzbi scry 3
 
     stzb curcol yellow
     stmb <scrx2 >scrx2 4
@@ -292,6 +292,25 @@ draw_credits:
     stmb <scrx2 >scrx2 17
     lday <txt_c4 >txt_c4
     call <print_string_ay >print_string_ay
+
+    addzbi scry 2
+    inczbi curchar
+    call <clear_curchar >clear_curchar
+
+    stzb curcol yellow
+    stmb <scrx2 >scrx2 4
+    lday <txt_c5 >txt_c5
+    call <print_string_ay >print_string_ay
+
+    addzbi scry 2
+    inczbi curchar
+    call <clear_curchar >clear_curchar
+
+    stzb curcol white
+    stmb <scrx2 >scrx2 12
+    lday <txt_c6 >txt_c6
+    call <print_string_ay >print_string_ay
+
     0
 
     rts
@@ -301,6 +320,8 @@ txt_c1:     @(string4x8 "CODE & GRAPHICS:") 255
 txt_c2:     @(string4x8 "SVEN MICHAEL KLOSE") 255
 txt_c3:     @(string4x8 "CHIP SOUNDS:") 255
 txt_c4:     @(string4x8 "ADRIAN FOX") 255
+txt_c5:     @(string4x8 "DOH GRAPHICS:") 255
+txt_c6:     @(string4x8 "MICHAEL KIRCHER") 255
 
 reset_framecounter:
     lda #0
