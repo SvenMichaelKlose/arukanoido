0efb07b4526af6c986b59686676f2b9bdf4a7ca8
Sven Michael Klose <pixel@hugbox.org>
2024-02-20 17:36:49 +0100

    Format to fit standard text width.

--- src/sprite-inits.asm
+++ src/sprite-inits.asm
@@ -24,13 +24,42 @@ multiwhite  = @(+ multicolor white)
 loaded_sprite_inits:
     org sprite_inits
 
-vaus_init:      is_vaus     0 0 @(+ 128 multiwhite) <gfx_vaus >gfx_vaus    <ctrl_vaus >ctrl_vaus 10 0 0 0
-ball_init:      is_ball     0 0 white      <gfx_ball >gfx_ball    <ctrl_ball >ctrl_ball 9 0 0 0
-laser_init:     is_laser    0 0 yellow     <gfx_laser >gfx_laser  <ctrl_laser >ctrl_laser 9 0 0 0
-bonus_init:     is_bonus    0 0 0          0 >gfx_bonus_l         <ctrl_bonus >ctrl_bonus 9 0 0 0
-obstacle_init:  is_obstacle 0 0 0          0 0                    <ctrl_obstacle_move_in >ctrl_obstacle_move_in 17 0 0 0
+vaus_init:
+    is_vaus
+    0 0 @(+ 128 multiwhite)
+    <gfx_vaus >gfx_vaus
+    <ctrl_vaus >ctrl_vaus
+    10 0 0 0
+ball_init:
+    is_ball
+    0 0 white
+    <gfx_ball >gfx_ball
+    <ctrl_ball >ctrl_ball
+    9 0 0 0
+laser_init:
+    is_laser
+    0 0 yellow
+    <gfx_laser >gfx_laser
+    <ctrl_laser >ctrl_laser
+    9 0 0 0
+bonus_init:
+    is_bonus
+    0 0 0
+    0 >gfx_bonus_l
+    <ctrl_bonus >ctrl_bonus
+    9 0 0 0
+obstacle_init:
+    is_obstacle
+    0 0 0
+    0 0
+    <ctrl_obstacle_move_in >ctrl_obstacle_move_in
+    17 0 0 0
 doh_obstacle_init:
-                is_doh_obstacle 0 0 yellow     <gfx_obstacle_doh  >gfx_obstacle_doh <ctrl_doh_obstacle >ctrl_doh_obstacle 9 0 0 0
+    is_doh_obstacle
+    0 0 yellow
+    <gfx_obstacle_doh >gfx_obstacle_doh
+    <ctrl_doh_obstacle >ctrl_doh_obstacle
+    9 0 0 0
 sprite_inits_end:
 
 sprite_inits_size = @(- sprite_inits_end sprite_inits)
