032061c1f513c488ce0a1a999c0bb27819dfdfdf
Sven Michael Klose <pixel@hugbox.org>
2024-02-20 15:22:49 +0100

    Knock off a couple of cycles.

--- src/reflect-ball-obstacle.asm
+++ src/reflect-ball-obstacle.asm
@@ -2,24 +2,21 @@ reflect_ball_obstacle:
     lda #0
     sta side_degrees
 
-reflect_ball_obstacle_h:
     ; Bounce back left.
     lda sprites_d,x         ; Moving to the left?
     bpl +n                  ; No…
     lda sprites_x,x
     cmp sprites_x,y
-    bcs +j
-    bcc reflect_ball_obstacle_v ; (jmp)
+    bcs +l
+j:  lda #64
+    sta side_degrees
+    rts
 
     ; Bounce back right.
 n:  lda sprites_x,x
     cmp sprites_x,y
-    bcs reflect_ball_obstacle_v
+    bcc -j
 
-j:  lda #64
-    bne +l                  ; (jmp)
-
-reflect_ball_obstacle_v:
     ; Bounce back from top.
     lda sprites_d,x         ; Are we flying upwards?
     clc
@@ -27,16 +24,13 @@ reflect_ball_obstacle_v:
     bpl +n                  ; No…
     lda sprites_y,x
     cmp sprites_y,y
-    bcs +j
-    rts
+    bcc +r
+j:  lda #128
+    sta side_degrees
+r:  rts
 
     ; Bounce back from bottom.
 n:  lda sprites_y,x
     cmp sprites_y,y
-    bcc +j
-    rts
-j:  lda #128
-l:  clc
-    adc side_degrees
-    sta side_degrees
+    bcc -j
     rts
