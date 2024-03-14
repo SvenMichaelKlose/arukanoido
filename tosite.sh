87acdedac26c05ea7d081977a1e2a1a408e8f222
Sven Michael Klose <pixel@hugbox.org>
2024-03-14 13:37:52 +0100

    Remove unused.

--- tosite.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cp -v arukanoido*.zip NEWS ~/Desktop/hugbox.org/www/pixel/software/vic-20/arukanoido/
-cp -v README.md ~/Desktop/hugbox.org/www/pixel/software/vic-20/arukanoido/README.txt
